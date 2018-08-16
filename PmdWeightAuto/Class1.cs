6771cf12062075058334aadbdb5f1fe330896577
massa-senohito <i7da.masa@gmail.com>
2018-08-16 23:24:16 +0900

    骨ローカル空間計算のテスト

--- PmdWeightAuto/Class1.cs
+++ PmdWeightAuto/Class1.cs
@@ -1,6 +1,8 @@
 ﻿using AttEditorWin;
 using PEPlugin;
 using PEPlugin.Pmx;
+using PEPlugin.SDX;
+using SlimDX;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -124,6 +126,7 @@ namespace PmdWeightAuto
 ウェイトを数値指定
 骨の向きから指向性
 距離分布図
+  max min から適切な大きさを導く　
 歪ませ距離分布図
        */
       UpdateAttEditor();
@@ -161,6 +164,17 @@ namespace PmdWeightAuto
     {
       attForm.SetAttr(GetVertAttribute().ToList());
     }
+    public void MainWindow()
+    {
+      IEnumerable<VertAttribute> verts = GetVertAttribute();
+      attForm = Program.RunForm(verts.ToList());
+      attForm.OnClickedCol = OnClickedVert;
+      form = DrawSpline.Program.SplineStarter(OnUpdate);
+      var form2 = DrawSpline.Program.SplineStarter(OnUpdate);
+      form2.Name = "Distribute";
+      var ps = verts.Select(v => new System.Drawing.Point((int)(v.Dist * 200.0), (int)(v.Weight * 200.0f))).ToList();
+      form2.SetPoints(ps);
+    }
 
     public override void Run(IPERunArgs args)
     {
@@ -168,13 +182,68 @@ namespace PmdWeightAuto
       Con = args.Host.Connector.Pmx;
       Pmx = Con.GetCurrentState();
       View = args.Host.Connector.View.PmxView;
-     // View.SetSelectedBoneIndices(new int[] { 0});
-     // View.UpdateView();
-
-      attForm = Program.RunForm(GetVertAttribute().ToList());
-      attForm.OnClickedCol = OnClickedVert;
-      form = DrawSpline.Program.SplineStarter(OnUpdate);
+      // View.SetSelectedBoneIndices(new int[] { 0});
+      // View.UpdateView();
+      var indices = View.GetSelectedBoneIndices();
+      for (int i = 0; i < indices.Length; i++)
+      {
+        var selBone = Pmx.Bone[indices[i]];
+        //foreach (var item in Pmx.Vertex)
+        //View.SetSelectedVertexIndices( VertexIndiceInRange(selBone,0.8f).ToArray() );
+      var pos = PmxUtil.BoneAttitude(selBone);
+        //pos.Decompose(out Vector3 scale, out Quaternion rot, out Vector3 loc);
+        //rot = Quaternion.Invert(rot);
+        var dir = (PmxUtil.TargetPosition(selBone) - selBone.Position);
+        dir.Normalize();
+        Pmx.Primitive.AddBox(0, dir * 0.8f * 5.0f, 0.8f, 0.8f, 0.8f, selBone);
+      }
+      Con.Update(Pmx);
+      View.UpdateModel_Vertex();
     }
+
+    private IEnumerable<int> VertexIndiceInRange(IPXBone selBone,float range)
+    {
+      for (int v = 0; v < Pmx.Vertex.Count; v++)
+      {
+        var item = Pmx.Vertex[v];
+        Vector3 vector3 = BoneLocalVerticePosition(selBone, item);
+        vector3.Z *= 0.2f;
+        if (vector3.Length() < range)
+        {
+          yield return v ;
+        }
+      }
+    }
+
+    private Vector3 BoneLocalVerticePosition(IPXBone selBone,IPXVertex item)
+    {
+      var pos = PmxUtil.BoneAttitude(selBone);
+      pos.Decompose(out Vector3 scale, out Quaternion rot, out Vector3 loc);
+      rot = Quaternion.Invert(rot);
+      //foreach (var item in Pmx.Vertex)
+      {
+        // なんかpositionなくなる
+        //item.Position -= selBone.Position;
+        //Vector4 vector4 = Vector3.Transform(item.Position, Matrix.RotationAxis(rot.Axis, rot.Angle));
+        //item.Position = new V3(vector4.X, vector4.Y, vector4.Z);
+        var tmp = item.Position - selBone.Position;
+        Vector4 vector4 = Vector3.Transform(tmp, Matrix.RotationAxis(rot.Axis, rot.Angle));
+        return new V3(vector4.X, vector4.Y, vector4.Z);
+
+      }
+    }
+
+    public static void Main(string[] args)
+    {
+      var pos = new Vector3(1, 0, 0);
+      var t = Matrix.RotationAxis(new Vector3(0, 0, 1), 3.141592f / 2.0f );
+      var transed = Vector3.Transform(pos, t);
+      Matrix right = PmxUtil.LookAt(new Vector3(0,0,0), new Vector3(10,0,0), new Vector3(0,1,0) );
+      //Matrix right = Matrix.LookAtLH(new Vector3(0,0,0), new Vector3(10,0,0), new Vector3(0,0,1) );
+      right.Decompose(out Vector3 scale, out Quaternion rot, out Vector3 loc);
+      Console.WriteLine("");
+    }
+
     private IPXPmx Pmx;
     private IPXPmxConnector Con;
     private PEPlugin.View.IPXPmxViewConnector View;
---
+++ PmdWeightAuto/PmxUtil.cs
@@ -0,0 +1,60 @@
+﻿using PEPlugin.Pmx;
+using PEPlugin.SDX;
+using SlimDX;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PmdWeightAuto
+{
+  static class PmxUtil
+  {
+
+    public static Matrix BoneAttitude(IPXBone bone)
+    {
+      V3 worldBoneTarget = TargetPosition(bone);
+      var toward = worldBoneTarget - bone.Position;
+      toward.Normalize();
+      var up = Vector3.UnitY;
+      if (Vector3.Dot(up, toward) > 0.8)
+      {
+        up = Vector3.UnitX;
+      }
+
+      Matrix right = Matrix.LookAtRH(bone.Position, worldBoneTarget, up);
+      right.Decompose(out Vector3 scale, out Quaternion rot, out Vector3 loc);
+      return Matrix.Translation(bone.Position) * right;
+    }
+
+    public static V3 TargetPosition(IPXBone bone)
+    {
+      var worldBoneTarget = new V3();
+      if (bone.ToBone != null)
+      {
+        worldBoneTarget = bone.ToBone.Position;
+      }
+      else
+      {
+        worldBoneTarget = bone.Position + bone.ToOffset;
+      }
+
+      return worldBoneTarget;
+    }
+
+    public static Matrix LookAt(Vector3 from , Vector3 to , Vector3 up)
+    {
+      Vector3 zAxis = to - from;
+      zAxis.Normalize();
+      Vector3 xAxis = Vector3.Cross(up, zAxis);
+      Vector3 yAxis = Vector3.Cross(zAxis, xAxis);
+      Matrix m = new Matrix();
+      m.set_Rows(0, new Vector4(xAxis, 0));
+      m.set_Rows(1, new Vector4(yAxis, 0));
+      m.set_Rows(2, new Vector4(zAxis, 0));
+      m.set_Rows(3, new Vector4(Vector3.Dot(xAxis, -from),Vector3.Dot(yAxis, -from), Vector3.Dot(zAxis, -from), 1));
+      return m;
+    }
+  }
+}
